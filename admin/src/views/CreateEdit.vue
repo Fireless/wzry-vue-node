b811e2de0fffaa555290672bec356159bb2d2933
Fireless <492429553@qq.com>
2019-10-14 22:04:46 +0800

    create the port on server

--- admin/src/views/CreateEdit.vue
+++ admin/src/views/CreateEdit.vue
@@ -1,8 +1,29 @@
 <template>
     <div class = "about">
     <h1>新建分类</h1>
-    <el-form>
-    
+    <el-form label-width="120px">
+    <el-form-item label= "名称">
+        <el-input v-model="model.name"></el-input>
+    </el-form-item>
+    <el-form-item>
+     <el-button type="primary" native-type="submit" @submit.native.prevent="save" >保存</el-button>
+    </el-form-item>
     </el-form>
     </div>
 </template>
+
+<script>
+    export default {
+        data(){
+            return {
+                model:{}
+            }
+        },
+
+        methods:{
+            save(){
+                // this.$http.post()
+            }
+        }
+    }
+</script>
