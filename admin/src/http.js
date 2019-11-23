66ad9fbe23950fd16e30219ccd4cc68dfbebbe48
Fireless <492429553@qq.com>
2019-11-23 22:04:15 +0800

    update

--- admin/src/http.js
+++ admin/src/http.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const http = axios.create({
    baseURL: process.env.VUE_APP_API_URL || '/admin/api', 
-   baseURL: 'http://localhost:3000/admin/api'
+   //baseURL: 'http://localhost:3000/admin/api'
 })
 
 export default http
